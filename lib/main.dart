13d9307f1e298a67e62b67daf455dd38ca35670d
ubitech4 <sohan@ubitechsolutions.com>
2019-04-04 10:51:28 +0530

    some change

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: "Calculate simple intrest",
+    title: "Calculate simple intrest abc",
     home: SIForm(),
     theme: ThemeData(
       brightness: Brightness.dark,
